a58c17a86ddf7b0db4ed769f25190bd75e3777c7
Yung Siang Liau <liauys@gmail.com>
2015-12-19 22:04:11 +0800

    Split main from lib

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,97 @@
 module Main where
 
-import Lib
+import           Control.Monad
+import           Data.List             (intercalate)
+import           Data.List.Split
+import           Data.String.Utils     (replace, strip)
+import           System.Console.GetOpt
+import           System.Environment
+import           System.Exit
+
+import           Lib
+
+prettyPrint :: Definition -> String
+prettyPrint def = intercalate "\n" $ map format makeLines
+  where
+    makeLines = [ "\nW O R D"
+                , "-------\n"
+                , word def
+                , "\nM E A N I N G"
+                , "-------------\n"
+                , meaning def
+                , "\nE X A M P L E"
+                , "-------------\n"
+                , example def
+                , "\nA U T H O R"
+                , "-----------\n"
+                , author def
+                , "\nV O T E S"
+                , "---------\n"
+                , "Up: " ++ show (up def) ++ " Down: " ++ show (down def)
+                , "\nD A T E"
+                , "-------\n"
+                , date def
+                , ""
+                ]
+    wrapline s = intercalate "\n" $ collect (words s) [] 0
+      where
+        collect (x:xs) ys c = case (length x) + c > 65 of
+          False -> collect xs (x:ys) (length x + c)
+          otherwise ->  (finalize ys) : (collect xs [x] (length x))
+        collect [] ys _ = [finalize ys]
+        finalize ys = intercalate " " (reverse ys)
+    format s = intercalate "\n" $ map wrapline paragraphs
+      where
+        paragraphs = splitOn "\n" (replace "\r" "" s)
+
+data Flag
+  = Define String
+  | Help
+  deriving (Show, Eq)
+
+options :: [OptDescr Flag]
+options =
+  [ Option ['d'] ["define"] (ReqArg Define "WORD") "Look up the definition of a word"
+  , Option ['h'] ["help"]   (NoArg Help) "Display help"
+  ]
+
+getUsageInfo :: String
+getUsageInfo = usageInfo header options
+  where header = "Usage: urbandict [OPTION...]"
+
+compilerOpts :: [String] -> IO ([Flag], [String])
+compilerOpts argv =
+  case getOpt Permute options argv of
+    (o,n,[]  ) -> return (o,n)
+    (_,_,errs) -> ioError (userError (concat errs ++ getUsageInfo))
+
+mainSearch :: String -> IO ()
+mainSearch w = do
+  putStrLn $ "Looking up the word [" ++ w ++ "]..."
+  src <- openURL (buildSearchUrl w)
+  let records = extractDefinitions src
+  putStrLn $ prettyPrint $ head records
+  --putStrLn $ show (head records)
+
+exitWithHelp :: IO ()
+exitWithHelp  = do
+  putStrLn $ getUsageInfo
+  exitWith ExitSuccess
 
 main :: IO ()
-main = someFunc
+main = do
+  (os, ns) <- getArgs >>= compilerOpts
+  -- putStrLn $ "Options: " ++ show os
+  -- putStrLn $ "Non-options: " ++ show ns
+  if Help `elem` os
+    then exitWithHelp
+    else
+      forM_ os $ \opt -> do
+        case opt of
+          Define w -> do
+            mainSearch w
+            exitWith ExitSuccess
+  if not . null $ ns
+    then mainSearch $ head ns
+    else exitWithHelp
+  return ()
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,14 +1,15 @@
 {-y LANGUAGE TemplateHaskell #-}
 
 module Lib
-    ( someFunc
-    , extractDefinitions
+    ( extractDefinitions
     , Definition (..)
     , defaultDefinition
     , extractNextPageUrlFromSource
     , validateDefinition
     , ValidationResult (..)
     , extractDefinitionPageUrls
+    , buildSearchUrl
+    , openURL
     ) where
 
 import           Control.Concurrent    (threadDelay)
@@ -16,15 +17,11 @@ import           Control.Monad
 import           Data.ByteString.Char8 (unpack)
 import           Data.ByteString.Lazy  (toStrict)
 import           Data.Char             (isDigit, toUpper)
-import           Data.List             (intercalate, isInfixOf, isPrefixOf,
-                                        replicate)
+import           Data.List             (isInfixOf, isPrefixOf, replicate)
 import           Data.List.Split
 import           Data.Maybe
 import           Data.String.Utils     (replace, strip)
 import           Network.HTTP.Conduit  (simpleHttp)
-import           System.Console.GetOpt
-import           System.Environment
-import           System.Exit
 import           Text.HTML.TagSoup
 import           Text.Regex
 
@@ -258,91 +255,3 @@ crawlFromAlphabet alphabet = do
 
 buildSearchUrl :: String -> String
 buildSearchUrl w = homeURL ++ "/define.php?term=" ++ w
-
-prettyPrint :: Definition -> String
-prettyPrint def = intercalate "\n" $ map format makeLines
-  where
-    makeLines = [ "\nW O R D"
-                , "-------\n"
-                , word def
-                , "\nM E A N I N G"
-                , "-------------\n"
-                , meaning def
-                , "\nE X A M P L E"
-                , "-------------\n"
-                , example def
-                , "\nA U T H O R"
-                , "-----------\n"
-                , author def
-                , "\nV O T E S"
-                , "---------\n"
-                , "Up: " ++ show (up def) ++ " Down: " ++ show (down def)
-                , "\nD A T E"
-                , "-------\n"
-                , date def
-                , ""
-                ]
-    wrapline s = intercalate "\n" $ collect (words s) [] 0
-      where
-        collect (x:xs) ys c = case (length x) + c > 65 of
-          False -> collect xs (x:ys) (length x + c)
-          otherwise ->  (finalize ys) : (collect xs [x] (length x))
-        collect [] ys _ = [finalize ys]
-        finalize ys = intercalate " " (reverse ys)
-    format s = intercalate "\n" $ map wrapline paragraphs
-      where
-        paragraphs = splitOn "\n" (replace "\r" "" s)
-
-data Flag
-  = Define String
-  | Help
-  deriving (Show, Eq)
-
-options :: [OptDescr Flag]
-options =
-  [ Option ['d'] ["define"] (ReqArg Define "WORD") "Look up the definition of a word"
-  , Option ['h'] ["help"]   (NoArg Help) "Display help"
-  ]
-
-getUsageInfo :: String
-getUsageInfo = usageInfo header options
-  where header = "Usage: urbandict [OPTION...]"
-
-compilerOpts :: [String] -> IO ([Flag], [String])
-compilerOpts argv =
-  case getOpt Permute options argv of
-    (o,n,[]  ) -> return (o,n)
-    (_,_,errs) -> ioError (userError (concat errs ++ getUsageInfo))
-
-mainSearch :: String -> IO ()
-mainSearch w = do
-  putStrLn $ "Looking up the word [" ++ w ++ "]..."
-  src <- openURL (buildSearchUrl w)
-  let records = extractDefinitions src
-  putStrLn $ prettyPrint $ head records
-  --putStrLn $ show (head records)
-
-exitWithHelp :: IO ()
-exitWithHelp  = do
-  putStrLn $ getUsageInfo
-  exitWith ExitSuccess
-
-main :: IO ()
-main = do
-  (os, ns) <- getArgs >>= compilerOpts
-  -- putStrLn $ "Options: " ++ show os
-  -- putStrLn $ "Non-options: " ++ show ns
-  if Help `elem` os
-    then exitWithHelp
-    else
-      forM_ os $ \opt -> do
-        case opt of
-          Define w -> do
-            mainSearch w
-            exitWith ExitSuccess
-  if not . null $ ns
-    then mainSearch $ head ns
-    else exitWithHelp
-  return ()
-
-someFunc = main
